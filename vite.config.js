a8aa34d1febf3be930c6a945f3447b825cac0b72
Rockhalt <adityavishwakarma@121007@gmail.com>
2026-06-22 18:11:09 +0530

    Fixed base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Website-Aniverse/',
   plugins: [react()],
+  base: '/', // <-- Change this to just a single slash for Vercel!
 })
