50cb0a5f9b9c987ebd7b21201e54fe9c7944de76
Rockhalt <adityavishwakarma@121007@gmail.com>
2026-06-22 18:22:03 +0530

    Removed router basename for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Website-Aniverse/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
